bf59a83727bf22934306ccbea5fd2384876254c8
Velasco Dendy <velascodendy2@gmail.com>
2026-06-20 13:51:59 +0700

    tambahan sedikit

--- 01_Python_dasar/variabel.py
+++ 01_Python_dasar/variabel.py
@@ -1,7 +1,16 @@
+#string
 Nama = "Velasco_dendy"
+
+#int
 Angkatan = 25
 semester = 3
 
+#float
+IPS = 1.89
+
 print(Nama)
 print(Angkatan)
-print(f"semester saya {3}, baru masuk")
+
+#f-string
+print(f"semester saya {semester}, baru masuk")
+print(f"IPS saya disemester 1 adalah {IPS}")
