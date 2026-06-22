0fe44eba231385f3ea485907f46d5839bfb73e94
Velasco Dendy <velascodendy2@gmail.com>
2026-06-22 17:27:31 +0700

    IO revisi

--- 01_Python_dasar/Input_Output.py
+++ 01_Python_dasar/Input_Output.py
@@ -1,23 +1,36 @@
-Nama = input("masukkan nama anda:\n")
-print(f"hallo {Nama}")
+# Nama = input("masukkan nama anda:\n")
+# print(f"hallo {Nama}")
 
-nama = "udin"
-umur = 20
-Kota = "Malang"
-print(nama, umur, Kota)
+# nama = "udin"
+# umur = 20
+# Kota = "Malang"
+# print(nama, umur, Kota)
 
-Y, K = input("masukkan 2 kota pilihan anda\n").split()
+# Y, K = input("masukkan 2 kota pilihan anda\n").split()
 
-print(f"pilihan kota anda adalah {Y} dan {K}")
+# print(f"pilihan kota anda adalah {Y} dan {K}")
 
 
-orang = int(input("masukkan umur orang ini:\n"))
+# orang = int(input("masukkan umur orang ini:\n"))
 
-if orang <= 0:
-    print("lah ini mah belum lahir _-")
-elif 0 < orang <= 3:
-    print("ini batita")
-elif 4 <= orang <= 5:
-    print("ini balita")
+# if orang <= 0:
+#     print("lah ini mah belum lahir _-")
+# elif 0 < orang <= 3:
+#     print("ini batita")
+# elif 4 <= orang <= 5:
+#     print("ini balita")
+# else:
+#     print("udah gede")
+
+# #Logical operator
+
+print("apakah vectra kuorum?")
+vectra =  130
+Terlambat =  True
+
+if vectra >= 150:
+    print("kelass")
+elif vectra >= 140 and Terlambat:
+    print("dievaluasi")
 else:
-    print("udah gede")
+    print("bubar rek")
