6fc0bf33801468b015c991b4e6b17c0a58086b56
Velasco Dendy <velascodendy2@gmail.com>
2026-06-22 19:15:56 +0700

    lash hari ini

--- 01_Python_dasar/Looping.py
+++ 01_Python_dasar/Looping.py
@@ -1,20 +1,19 @@
-#for loop
-#n = 15 
-#for i in range (0, n):
+# for loop
+# n = 15 
+# for i in range (0, n):
 #    print(i)
 
 
-# List_Barang = ["esp32", "arduino", "rasberry" ]
-# for i in range(0, len()):
-#     print(i)
+List_Barang = ["esp32", "arduino", "rasberry" ]
+for i in range(0, len()):
+    print(i)
 
-# while n > 10:
-#     print("saya Komjar")
-#     # n =- 1
+while n > 10:
+    print("saya Komjar")
+    # n =- 1
 
-# x = range(3, 20, 2)
-# for n in x:
-#   print(n)
+x = range(3, 20, 2)
+for n in x:
+  print(n)
 
-#  Umur Balita
 
